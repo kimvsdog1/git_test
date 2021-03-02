fe179ad8a9ab383cdc46a587c628612baba0034e
dokyung <kimvsdog@aaaa.com>
2021-03-02 19:47:17 +0900

    프로젝트 생성

--- src/main/MainClass.java
+++ src/main/MainClass.java
@@ -1,10 +1,25 @@
 package main;
 
+import java.util.Scanner;
+
 public class MainClass {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("main인 시작");
+		// kim - 로그인관련 기능 만드세요
+		// cho - 회원관리 기능을 만드세요
+		Scanner input = new Scanner(System.in);
+		int num;
+		while(true) {
+			System.out.println("1.로그인 2.회원관리");
+			num = input.nextInt();
+			switch(num) {
+			case 1:
+				break;
+			case 2:
+				break;
+			}
+		}
+		
 	}
 
 }
